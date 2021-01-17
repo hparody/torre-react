b43519d7cf545cb796780a74784d81e443a2a1f4
Hemel Parody <hemelparody@hotmail.com>
2021-01-17 00:46:25 -0500

    React Project Created

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <h3>It's me, Hemel</h3>
+    </div>
+  );
+}
+
+export default App;
