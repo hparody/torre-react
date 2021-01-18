3636311164a9245dc059076a246dfb059ba6c918
Hemel Parody <hemelparody@hotmail.com>
2021-01-17 21:18:12 -0500

    [Feature]: Final Home Page

--- src/Containers/Home/Home.js
+++ src/Containers/Home/Home.js
@@ -1,14 +1,65 @@
 import React from "react";
-import { Button } from "antd";
+import { Button, Row, Col, Typography } from "antd";
+import HomeImage from "../../Assets/Images/torre-logo.png";
+import { useHistory } from "react-router-dom";
+import "./Home.less";
+import {
+	UserOutlined,
+	AimOutlined,
+} from "@ant-design/icons";
+const { Title } = Typography;
 
 const Home = (props) => {
+	const history = useHistory();
 	return (
 		<React.Fragment>
-			<h1>Hello World</h1>
-			<h3>It's me, Hemel at Home</h3>
-			<Button type="primary">Button</Button>
+			<Row className="homepage-container" justify="center" align="middle">
+				<Col span={20}>
+					<Row justify="center" className="align-center-h">
+						<Col xs={24}>
+							<img
+								className="homepage-img"
+								src={HomeImage}
+								alt={"Homepage"}
+							></img>
+							<Title>The Next-Generation Professional Network</Title>
+						</Col>
+					</Row>
+					<Row justify="center" className="align-center-h homepage-btn-section">
+						<Col xs={6} span={6}>
+							<Button
+								className="homepage-btn"
+								type="primary"
+								shape="round"
+								icon={<UserOutlined />}
+								size={"large"}
+								onClick={() => redirectTo(history, "users")}
+							>
+								<span className="btn-text-title">Users</span>
+							</Button>
+						</Col>
+						<Col xs={6} span={6}>
+							<Button
+								className="homepage-btn"
+								type="primary"
+								shape="round"
+								icon={<AimOutlined />}
+								size={"large"}
+								onClick={() => redirectTo(history, "jobs")}
+							>
+								<span className="btn-text-title">Jobs</span>
+							</Button>
+						</Col>
+					</Row>
+				</Col>
+			</Row>
 		</React.Fragment>
 	);
 };
+
+const redirectTo = (history, section) => {
+	history.push("/" + section);
+	console.log("Done");
+}
 
 export default Home;
--- src/Containers/Users/Users.js
+++ src/Containers/Users/Users.js
@@ -1,12 +1,34 @@
 import React from "react";
 import { Button } from "antd";
+import axios from "axios";
+
+async function searchUsers(evt) {
+	// console.log(evt);
+	const response = await axios({
+		method: "post",
+		url:
+			"https://search.torre.co/opportunities/_search/?offset=0&size=100&aggregate=false",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		data: {
+			"skill/role": {
+				text: "web development",
+				experience: "potential-to-develop",
+			},
+		},
+	});
+    console.log(response);
+}
 
 const Users = (props) => {
 	return (
 		<React.Fragment>
 			<h1>Hello World</h1>
 			<h3>It's me, Hemel at Users</h3>
-			<Button type="primary">Button</Button>
+			<Button type="primary" onClick={searchUsers}>
+				Button
+			</Button>
 		</React.Fragment>
 	);
 };
